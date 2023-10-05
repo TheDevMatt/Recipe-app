591bc763cffe392a745bd95d9c74db1fcde8cfe0
Mateusz Kot <mateuszkot@MacBook-Air-Mateusz.local>
2023-10-06 00:52:32 +0200

    Update of images

--- src/components/Data/data.js
+++ src/components/Data/data.js
@@ -1,281 +1,274 @@
-import Shawarma from '../images/ChickenShawarma.jpeg';
-import FruitJuice from '../images/FruitJuice.jpeg';
-import YogurtParfaite from '../images/YogurtParfaite.jpeg';
-import OrangeJuice from '../images/OrangeJuice.jpeg';
-import EggPlantain from '../images/Egg&Plantain.jpeg';
-import TilapiaFish from '../images/TilapiaFish.jpeg';
-import JollofRice from '../images/JollofRice.jpeg';
-import ChickenBurger from '../images/ChickenBurger.jpeg';
-import AfricanSalad from '../images/AfricanSalad.jpeg';
-import ChinesePizza from '../images/ChinesePizza.jpeg';
-import TomatoSoup from '../images/TomatoSoup.jpeg';
-import Devolay from '../images/Devolay.jpeg';
-import Dumplings from '../images/Dumplings.jpeg';
-import NeapolitanPizza from '../images/NeapolitanPizza.jpeg';
-import Bruschetta from '../images/Bruschetta.jpeg';
-import CheeselessPizza from '../images/CheeselessPizza.jpeg';
-import SupremePizza from '../images/SupremePizza.jpeg';
-import PepperoniPizza from '../images/PepperoniPizza.jpeg';
-import CheesePizza from '../images/CheesePizza.jpeg';
-import KaleSalad from '../images/KaleSalad.jpeg';
-import CeasarSalad from '../images/CeasarSalad.jpeg';
-import LoadedSalad from '../images/LoadedSalad.jpeg';
-import Wings from '../images/Wings.jpeg';
-import RoastedChicken from '../images/RoastedChicken.jpeg';
-import ChickenTenders from '../images/ChickenTenders.jpeg';
+import Shawarma from "../images/ChickenShawarma.jpeg";
+import FruitJuice from "../images/FruitJuice.jpeg";
+import YogurtParfaite from "../images/YogurtParfaite.jpeg";
+import OrangeJuice from "../images/OrangeJuice.jpeg";
+import EggPlantain from "../images/Egg&Plantain.jpeg";
+import TilapiaFish from "../images/TilapiaFish.jpeg";
+import JollofRice from "../images/JollofRice.jpeg";
+import ChickenBurger from "../images/ChickenBurger.jpeg";
+import AfricanSalad from "../images/AfricanSalad.jpeg";
+import ChinesePizza from "../images/ChinesePizza.jpeg";
+import TomatoSoup from "../images/TomatoSoup.jpeg";
+import Devolay from "../images/Devolay.jpeg";
+import Dumplings from "../images/Dumplings.jpeg";
+import NeapolitanPizza from "../images/NeapolitanPizza.jpeg";
+import Bruschetta from "../images/Bruschetta.jpeg";
+import CheeselessPizza from "../images/CheeselessPizza.jpeg";
+import SupremePizza from "../images/SupremePizza.jpeg";
+import PepperoniPizza from "../images/PepperoniPizza.jpeg";
+import CheesePizza from "../images/CheesePizza.jpeg";
+import KaleSalad from "../images/KaleSalad.jpeg";
+import CeasarSalad from "../images/CeasarSalad.jpeg";
+import LoadedSalad from "../images/LoadedSalad.jpeg";
+import Wings from "../images/Wings.jpeg";
+import RoastedChicken from "../images/RoastedChicken.jpeg";
+import ChickenTenders from "../images/ChickenTenders.jpeg";
+import BurgerCategory from "../images/BurgerCategory.jpeg";
+import VegeCategory from "../images/VegetablesCategory.jpeg";
+import PizzaCategory from "../images/PizzaCategory.jpeg";
+import WingsCategory from "../images/WingsCategory.jpeg";
+import ShawarmaCategory from "../images/ShawarmaCategory.jpeg";
+import RisottoCategory from "../images/RisottoCategory.jpeg";
+import CakeCategory from "../images/CakeCategory.jpeg";
+import ChickenCategory from "../images/ChickenCategory.jpeg";
+import IceCreamCategory from "../images/IceCreamCategory.jpeg";
+import BeefCategory from "../images/BeefCategory.jpeg";
+import ApplePieCategory from "../images/ApplePieCategory.jpeg";
 
 export const topPicks = [
   {
     id: 1,
-    title: 'Shawarma',
-    price: '$10',
-    img: Shawarma
+    title: "Shawarma",
+    price: "$10",
+    img: Shawarma,
   },
   {
     id: 2,
-    title: 'Fruit Juice',
-    price: '$5',
+    title: "Fruit Juice",
+    price: "$5",
     img: FruitJuice,
   },
   {
     id: 3,
-    title: 'Yogurt Parfaite',
-    price: '$3',
+    title: "Yogurt Parfaite",
+    price: "$3",
     img: YogurtParfaite,
   },
   {
     id: 4,
-    title: 'Orange Juice',
-    price: '$5',
+    title: "Orange Juice",
+    price: "$5",
     img: OrangeJuice,
   },
   {
     id: 5,
-    title: 'Egg & Plantain',
-    price: '$8',
+    title: "Egg & Plantain",
+    price: "$8",
     img: EggPlantain,
   },
   {
     id: 6,
-    title: 'Tilapia Fish',
-    price: '$10',
+    title: "Tilapia Fish",
+    price: "$10",
     img: TilapiaFish,
   },
   {
     id: 7,
-    title: 'Jollof Rice',
-    price: '$9',
+    title: "Jollof Rice",
+    price: "$9",
     img: JollofRice,
   },
   {
     id: 8,
-    title: 'Chicken Burger',
-    price: '$15',
+    title: "Chicken Burger",
+    price: "$15",
     img: ChickenBurger,
   },
   {
     id: 9,
-    title: 'African Salad',
-    price: '$7',
-    img:AfricanSalad,
+    title: "African Salad",
+    price: "$7",
+    img: AfricanSalad,
   },
   {
     id: 10,
-    title: 'Chinese Pizza',
-    price: '$12',
-    img:ChinesePizza,
+    title: "Chinese Pizza",
+    price: "$12",
+    img: ChinesePizza,
   },
   {
-    id:11,
-    title:'Tomato Soup',
-    price: '$9',
+    id: 11,
+    title: "Tomato Soup",
+    price: "$9",
     img: TomatoSoup,
   },
   {
-    id:12,
-    title:'Devolay',
-    price:'$18',
+    id: 12,
+    title: "Devolay",
+    price: "$18",
     img: Devolay,
   },
   {
-    id:13,
-    title:'Dumplings',
-    price:'$15',
+    id: 13,
+    title: "Dumplings",
+    price: "$15",
     img: Dumplings,
   },
   {
-    id:14,
-    title:'Neapolitan pizza',
-    price: '$14',
+    id: 14,
+    title: "Neapolitan pizza",
+    price: "$14",
     img: NeapolitanPizza,
   },
   {
-    id:15,
-    title:'Bruschetta',
-    price: '$9',
+    id: 15,
+    title: "Bruschetta",
+    price: "$9",
     img: Bruschetta,
-  }
+  },
 ];
 
 export const mealData = [
   {
-    id: 5,
-    name: 'Cheeseless Pizza',
-    category: 'pizza',
-    img: CheeselessPizza,
-    price: '$16',
-  },
-  {
-    id: 6,
-    name: 'Supreme Pizza',
-    category: 'pizza',
-    img: SupremePizza,
-    price: '$25',
-  },
-  {
-    id: 7,
-    name: 'Pepperoni Pizza',
-    category: 'pizza',
-    img: PepperoniPizza,
-    price: '$22',
-  },
-  {
-    id: 8,
-    name: 'Cheese Pizza',
-    category: 'pizza',
-    img: CheesePizza,
-    price: '$20',
-  },
-  {
-    id: 9,
-    name: 'African Salad',
-    category: 'Salad',
-    img: AfricanSalad,
-    price: '$7',
-  },
-  {
-    id: 10,
-    name: 'Kale Salad',
-    category: 'salad',
-    img: KaleSalad,
-    price: '$8',
-  },
-  {
-    id: 11,
-    name: 'Ceasar Salad',
-    category: 'salad',
-    img: CeasarSalad,
-    price: '$9',
-  },
-  {
-    id: 12,
-    name: 'Loaded Salad',
-    category: 'salad',
-    img:LoadedSalad,
-    price: '$11',
-  },
-  {
-    id: 13,
-    name: 'Chicken Shawarma',
-    category: 'chicken',
-    img:Shawarma,
-    price: '$15',
-  },
-  {
-    id: 14,
-    name: 'Wings',
-    category: 'chicken',
-    img:Wings,
-    price: '$16',
-  },
-  {
-    id: 15,
-    name: 'Roasted Chicken',
-    category: 'chicken',
-    img:RoastedChicken,
-    price: '$18',
-  },
-  {
     id: 16,
-    name: 'Chicken Tenders',
-    category: 'chicken',
+    name: "Cheeseless Pizza",
+    category: "Pizza",
+    img: CheeselessPizza,
+    price: "$16",
+  },
+  {
+    id: 17,
+    name: "Supreme Pizza",
+    category: "Pizza",
+    img: SupremePizza,
+    price: "$25",
+  },
+  {
+    id: 18,
+    name: "Pepperoni Pizza",
+    category: "Pizza",
+    img: PepperoniPizza,
+    price: "$22",
+  },
+  {
+    id: 19,
+    name: "Cheese Pizza",
+    category: "Pizza",
+    img: CheesePizza,
+    price: "$20",
+  },
+  {
+    id: 20,
+    name: "African Salad",
+    category: "Salad",
+    img: AfricanSalad,
+    price: "$7",
+  },
+  {
+    id: 21,
+    name: "Kale Salad",
+    category: "Salad",
+    img: KaleSalad,
+    price: "$8",
+  },
+  {
+    id: 22,
+    name: "Ceasar Salad",
+    category: "Salad",
+    img: CeasarSalad,
+    price: "$9",
+  },
+  {
+    id: 23,
+    name: "Loaded Salad",
+    category: "Salad",
+    img: LoadedSalad,
+    price: "$11",
+  },
+  {
+    id: 24,
+    name: "Chicken Shawarma",
+    category: "Chicken",
+    img: Shawarma,
+    price: "$15",
+  },
+  {
+    id: 25,
+    name: "Wings",
+    category: "Chicken",
+    img: Wings,
+    price: "$16",
+  },
+  {
+    id: 26,
+    name: "Roasted Chicken",
+    category: "Chicken",
+    img: RoastedChicken,
+    price: "$18",
+  },
+  {
+    id: 27,
+    name: "Chicken Tenders",
+    category: "Chicken",
     img: ChickenTenders,
-    price: '$20',
+    price: "$20",
   },
 ];
 
 export const categories = [
-    {
-      id: 1,
-      name: 'Burger',
-      image:
-         'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672070207/NetflixApp/burger_logo_hmpqua.png'
-    },
-    {
-      id: 2,
-      name: 'Vege',
-      image:
-        'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672070207/NetflixApp/veg_logo_jvm4qn.png'
-    },
-    {
-      id: 3,
-      name: 'Pizza',
-      image:
-        'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672070207/NetflixApp/pizza_logo_dqdqds.png',
-    },
-    {
-      id: 4,
-      name: 'Wings',
-      image:
-        'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672070208/NetflixApp/wings_logo_hrn8zl.png'
-    },
-
-    {
-      id: 5,
-      name: 'Best Overall',
-      image:
-        'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672070207/NetflixApp/fastfood_logo_v80nbk.png'
-    },
-    {
-      id: 6,
-      name: 'Sharwama',
-      image:
-        'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672070207/NetflixApp/sharwama_logo_st4vg8.png'
-    },
-    {
-      id: 7,
-      name: 'Resottoa',
-      image:
-        'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672176749/NetflixApp/resotto_logo_uywrr1.png'
-    },
-    {
-      id: 8,
-      name: 'Cake',
-      image:
-        'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672176749/NetflixApp/cake_edpoec.png'
-    },
-    {
-      id: 9,
-      name: 'Chicken',
-      image:
-        'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672176749/NetflixApp/chiken_logo_ahl2ca.png'
-    },
-    {
-      id: 10,
-      name: 'Ice Cream',
-      image:
-        'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672176749/NetflixApp/ice_cream_logo_hcqcaa.png'
-    },
-    {
-      id: 11,
-      name: 'Beef',
-      image:
-       'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672176749/NetflixApp/beef_logo_adnidz.png'
-    },
-    {
-      id: 12,
-      name: 'Apple Pie',
-      image:
-        'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672176749/NetflixApp/appple_pie_yxfxpm.png'
-    },
-  ];
+  {
+    id: 28,
+    name: "Burger",
+    img: BurgerCategory,
+  },
+  {
+    id: 29,
+    name: "Vege",
+    img: VegeCategory,
+  },
+  {
+    id: 30,
+    name: "Pizza",
+    img: PizzaCategory,
+  },
+  {
+    id: 31,
+    name: "Wings",
+    img: WingsCategory,
+  },
+  {
+    id: 32,
+    name: "Shawarma",
+    img: ShawarmaCategory,
+  },
+  {
+    id: 33,
+    name: "Resottoa",
+    img: RisottoCategory,
+  },
+  {
+    id: 34,
+    name: "Cake",
+    img: CakeCategory,
+  },
+  {
+    id: 35,
+    name: "Chicken",
+    img: ChickenCategory,
+  },
+  {
+    id: 36,
+    name: "Ice Cream",
+    img: IceCreamCategory,
+  },
+  {
+    id: 37,
+    name: "Beef",
+    img: BeefCategory,
+  },
+  {
+    id: 38,
+    name: "Apple Pie",
+    img: ApplePieCategory,
+  },
+];
