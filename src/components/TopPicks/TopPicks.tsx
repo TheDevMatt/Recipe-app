a4f3608416beb735da30e95db1970b26c3bc2c16
Mateusz Kot <mateuszkot@MacBook-Air-Mateusz.local>
2023-10-08 18:40:03 +0200

    Update of TopPicks

--- src/components/TopPicks/TopPicks.tsx
+++ src/components/TopPicks/TopPicks.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import {topPicks} from '../Data/Data';
-import { handleMouseAction } from '../ActionMethods/ActionMethods';
-import '../../styles/TopPicks.scss';
+import React from "react";
+import { topPicks } from "../Data/Data";
+import { handleMouseAction } from "../ActionMethods/ActionMethods";
+import "../../styles/TopPicks.scss";
 
 const TopPicks: React.FC = () => {
-
-    return(
-        <div className='top-picks'>
-            <h2>Top Picks</h2>
-            <div className='top-picks-items'>
-                {topPicks.map((item) => (
-                <div key={item.id} id={`meal-item-${item.id}`} className='meal-item' onMouseEnter={() => handleMouseAction(item.id, "enter")} onMouseLeave={() => handleMouseAction(item.id, "leave")}>
-                <img className='top-picks-image' src={item.img} alt={item.title}/>
-                <p className='meal-name-tooltip'>{item.title}<span>{item.price}</span></p>
-                </div>
-            ))}
-            </div>
-        </div>    
-        )
-}
+  return (
+    <div className="top-picks">
+      <h2>Top Picks</h2>
+      <p>
+        These meals are a carefully crafted assortment of dishes that represent
+        our signature flavors and creativity.
+      </p>
+      <div className="top-picks-items">
+        {topPicks.map((item) => (
+          <div
+            key={item.id}
+            id={`meal-item-${item.id}`}
+            className="meal-item"
+            onMouseEnter={() => handleMouseAction(item.id, "enter")}
+            onMouseLeave={() => handleMouseAction(item.id, "leave")}
+          >
+            <img className="top-picks-image" src={item.img} alt={item.title} />
+            <p className="meal-name-tooltip">
+              {item.title}
+              <span>{item.price}</span>
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TopPicks;
